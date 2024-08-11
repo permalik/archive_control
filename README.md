3967b4543abe32801cd694cc255fe875ffe6ec71
permalik <permalik@protonmail.com>
2024-08-11 02:04:41 +0000

    chore update todo

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # archive_control
-
-<!TODO: update email>
-set mail in linux server to email/locally mail errors from cron
--- shell/archive.sh
+++ shell/archive.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO: update email
+# set mail in linux server to email/locally mail errors from cron
+
 ansible_cmd="/home/tmalik/.local/bin/ansible-playbook"
 playbook_one="/home/tmalik/ansible/inventory/playbook_001.yml"
 playbook_two="/home/tmalik/ansible/inventory/playbook_002.yml"
